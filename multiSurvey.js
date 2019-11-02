2a7f264104a1a2cc1902c027c82e3a1c46a12fb1
Julie <julienne.bay@gmail.com>
2019-11-02 15:16:11 -0400

    clear old question/answers when new question is displayed

--- multiSurvey.js
+++ multiSurvey.js
@@ -1,6 +1,5 @@
 function loadScript() {
-  //quiz and result containers
-
+  //result container
   const resultContainer = document.querySelector("#results");
   resultContainer.hidden = true;
   //buttons
@@ -12,21 +11,26 @@ function loadScript() {
   nextButton.hidden = true;
   submitButton.hidden = true;
 
+  //questions and answers stored in an array of objects
   const questions = [
     { text: "question 1", answers: ["answer 11", "answer 12", "answer 13"] },
     { text: "question 2", answers: ["answer 21", "answer 22"] },
-    { text: "question 3", answers: ["answer 31"] }
+    { text: "question 3", answers: ["answer 31", "answer 32"] }
   ];
-
+  //this will tally up the score
   let index = 0;
-
   function startQuiz() {
     startButton.hidden = true;
     nextButton.hidden = false;
     const question = questions[index];
     const questionContainer = document.querySelector(".question");
+    //clear old questions
+    Array.from(questionContainer.children).forEach(child => child.remove());
+    //setting text Content in div without adding paragraphs
     questionContainer.textContent = question.text;
     const answerContainer = document.querySelector(".answers");
+    //clear old answers
+    Array.from(answerContainer.children).forEach(child => child.remove());
     const answers = question.answers;
     //this (below) draws the answers
     answers.forEach(a => {
